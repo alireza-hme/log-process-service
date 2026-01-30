c0923c7497ac2c99dd5abb0ebfb069c720f6e9bc
alireza hme <hme.alireza@gmail.com>
2026-01-30 17:52:35 +0330

    refactor(log_process): add httpx package & replace request with it

--- log_generator/src/main.py
+++ log_generator/src/main.py
@@ -1,58 +1,59 @@
 from fastapi import FastAPI
-from fastapi.background import BackgroundTasks
 import asyncio
 import os
-import time
-import random
 import json
+import random
 import faker
-import requests
 from datetime import datetime
-
+import httpx
 
 app = FastAPI()
 
-#Log file path
 LOG_FILE_PATH = "/app/logs/logs.json"
 os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
 
-def generate_logs(count=20):
-    fake = faker.Faker()
-    logs = []
+fake = faker.Faker()
 
+def generate_logs(count=20):
+    logs = []
     for _ in range(count):
         logs.append({
             "timestamp": datetime.now().isoformat(),
             "ip": fake.ipv4(),
             "user_agent": fake.user_agent(),
-            "status": random.choice([200, 500, 404]),
+            "status": random.choice([200, 404, 500]),
             "message": fake.sentence()
         })
-    with open(LOG_FILE_PATH, "a") as file:
-        json.dump(logs, file)
-        file.write("\n")
 
-def send_logs():
+    with open(LOG_FILE_PATH, "a") as f:
+        for log in logs:
+            f.write(json.dumps(log) + "\n")
+
+async def send_logs():
+    if not os.path.exists(LOG_FILE_PATH):
+        return
+
     try:
-        with open(LOG_FILE_PATH, "r") as file:
-            logs = [json.loads(line) for line in file]
+        with open(LOG_FILE_PATH, "r") as f:
+            logs = [json.loads(line) for line in f if line.strip()]
 
-        # Send logs to log_process via REST API
-        response = requests.post("http://log_process:5001/logs", json=logs)
-        response.raise_for_status()
+        if not logs:
+            return
 
-        # Clear the file
+        async with httpx.AsyncClient() as client:
+            await client.post("http://log_process:5001/logs", json=logs)
+
         open(LOG_FILE_PATH, "w").close()
+
     except Exception as e:
-        print("Error Sending Logs:",e)
+        print("Send error:", e)
 
-
-async def schedule_send_logs():
+async def scheduler():
     while True:
-        await asyncio.sleep(300) # 5 minutes
-        send_logs()
+        generate_logs()
+        await send_logs()
+        await asyncio.sleep(300)  # 5 minutes
 
 @app.on_event("startup")
-async def setup_periodic_task():
-    # Run send_logs every 5 minutes
-    asyncio.create_task(schedule_send_logs())
+async def startup():
+    asyncio.create_task(scheduler())
